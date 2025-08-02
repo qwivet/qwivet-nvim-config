56abe94fc349a968695d6519694af451e45c1c23
qwivet <qwivet@gmail.com>
2025-08-02 03:13:41 +0300

    git blame added
    added a git blame with 2 toggles

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,19 +1,35 @@
 return {
-  "kdheepak/lazygit.nvim",
-  lazy = true,
-  cmd = {
-    "LazyGit",
-    "LazyGitConfig",
-    "LazyGitCurrentFile",
-    "LazyGitFilter",
-    "LazyGitFilterCurrentFile",
-  },
+	{
+		"FabijanZulj/blame.nvim",
+		lazy = false,
+		opts = {
+			focus_blame = false,
+		},
+		config = function()
+			require("blame").setup({})
+		end,
+		keys = {
+			{ "<leader>th", "<cmd>BlameToggle<cr>", desc = "Toggle git file history" },
+			{ "<leader>tv", "<cmd>BlameToggle virtual<cr>", desc = "Toggle git file virtual history" },
+		},
+	},
+	{
+		"kdheepak/lazygit.nvim",
+		lazy = true,
+		cmd = {
+			"LazyGit",
+			"LazyGitConfig",
+			"LazyGitCurrentFile",
+			"LazyGitFilter",
+			"LazyGitFilterCurrentFile",
+		},
 
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
 
-  keys = {
-    { "<leader>tg", "<cmd>LazyGit<cr>", desc = "Open LazyGit" },
-  },
+		keys = {
+			{ "<leader>tg", "<cmd>LazyGit<cr>", desc = "Open LazyGit" },
+		},
+	},
 }
