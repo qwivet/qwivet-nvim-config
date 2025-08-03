3081c770e47bd35e56931139365092e341ef6cf3
qwivet <qwivet@gmail.com>
2025-08-03 18:04:00 +0300

    add cursor animation and folds

--- lua/plugins/utility.lua
+++ lua/plugins/utility.lua
@@ -1,12 +1,25 @@
 return {
-	{
-		"otavioschwanck/cool-substitute.nvim",
-		config = function()
-			require("cool-substitute").setup({})
-		end,
-	},
-	{
-		"karb94/neoscroll.nvim",
-		opts = { hide_cursor = false, duration_multiplier = 0.5, easing = "quadratic" },
-	},
+  {
+    "otavioschwanck/cool-substitute.nvim",
+    config = function()
+      require("cool-substitute").setup({})
+    end,
+  },
+  {
+    "karb94/neoscroll.nvim",
+    opts = { hide_cursor = false, duration_multiplier = 0.5, easing = "quadratic" },
+  },
+  {
+    "sphamba/smear-cursor.nvim",
+    opts = { cursor_color = "#514965" },
+  },
+  {
+    "pseewald/vim-anyfold",
+    config = function()
+      vim.cmd("filetype plugin indent on")
+      vim.cmd("syntax on")
+      vim.cmd("autocmd Filetype * AnyFoldActivate")
+      vim.cmd("set foldlevel=99")
+    end,
+  },
 }
